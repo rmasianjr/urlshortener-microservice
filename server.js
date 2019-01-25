0770a33cc14c846189ae4c451139bd7c0001f9fd
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2019-01-25 13:54:00 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.json({ error: 'Not Found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500);
+  res.json({ error: err.message });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
